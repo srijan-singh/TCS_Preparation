37f6f5f5f435d3cfd8a828fe78fe34f1f36fe060
Srijan Singh <srijanssj5goku@gmail.com>
2022-07-15 20:37:06 +0530

    sol6

---
+++ sol6.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+char odd_color(int size, vector<char> arr)
+{
+     char result;
+
+     memo
+
+     return result; 
+}
+
+int main()
+{
+     int size = 7;
+
+     vector<char> colors = {
+          'r','g','b','b','g','y','y'
+     };
+
+     return 0;
+}
